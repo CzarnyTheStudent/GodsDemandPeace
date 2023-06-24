4e53943ca53508a5d0b16c4c8584aa43b8001916
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2023-06-24 05:47:04 +0200

    DiceGame

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Dice : MonoBehaviour 
+{
+    public int cubeValue;
+    public GameObject[] objectsToCompare;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+        // Wyświetlenie nazwy najwyższego obiektu
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity == new Vector3(0, 0, 0))
+        {
+            GameObject highestObject = GetHighestObject(objectsToCompare);
+            cubeValue = highestObject.GetComponent<Value>().valueY;
+            Debug.Log("Najwyższy obiekt: " + highestObject.name);
+        }
+    }
+    
+    private GameObject GetHighestObject(GameObject[] objects)
+    {
+        GameObject highestObject = null;
+        float highestY = float.MinValue;
+
+        // Iteracja przez wszystkie obiekty
+        foreach (GameObject obj in objects)
+        {
+            // Sprawdzenie pozycji Y obiektu
+            float objY = obj.transform.position.y;
+
+            // Porównanie z dotychczasowym najwyższym obiektem
+            if (objY > highestY)
+            {
+                highestY = objY;
+                highestObject = obj;
+            }
+        }
+
+        return highestObject;
+    }
+}
