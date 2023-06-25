681997b870a9c14fb158f4b564e89733a8335c62
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2023-06-25 03:08:54 +0200

    DIIIIIICEEEEEEE

--- Assets/Scripts/DiceManager.cs
+++ Assets/Scripts/DiceManager.cs
@@ -10,31 +10,40 @@ public class DiceManager : MonoBehaviour
     private void Awake()
     {
         mainCamera = Camera.main;
-     
     }
 
     private void Update()
     {
-
         if (Input.GetMouseButtonUp(1))
         {
-                ThrowDice();
-            isThrowed= true;
+            ThrowDice();
+            isThrowed = true;
         }
     }
-
 
     private void ThrowDice()
     {
         if (!isThrowed)
         {
-            foreach (GameObject dice in diceArray) 
+            foreach (GameObject dice in diceArray)
             {
-                //dice.GetComponent<Rigidbody>().isKinematic = false;
                 dice.transform.SetParent(null);
-                dice.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * Random.Range(700f, 1000f));
-                dice.GetComponent<Rigidbody>().AddTorque(Random.insideUnitSphere * 200f);
-            
+
+                // Wylosuj losowy kąt w zakresie od 0 do 360 stopni
+                float randomAngle = Random.Range(-0.8f, 0.8f);
+
+                // Zamień losowy kąt na wektor kierunku na płaszczyźnie XY
+                Vector3 randomDirection = new Vector3(randomAngle, 0.2f, 1f);
+
+                Rigidbody diceRigidbody = dice.GetComponent<Rigidbody>();
+
+                // Zresetuj prędkość i moment obrotowy kostki
+                diceRigidbody.velocity = Vector3.zero;
+                diceRigidbody.angularVelocity = Vector3.zero;
+
+                //diceRigidbody.isKinematic = false;
+                diceRigidbody.AddForce(randomDirection * Random.Range(300f, 900f));
+                diceRigidbody.AddTorque(Random.insideUnitSphere * 100f);
             }
         }
     }
